5bd8f3747aa2ec52ecb6b2005017752eb59e2270
Charlike Mike Reagent <mameto2011@gmail.com>
2018-11-12 14:59:33 +0200

    major: release v5 (#66)
    major: run update, refactor to use [@tunnckocore/release-cli](https://github.com/tunnckoCore/release-cli)
    
    Hoping, if successfully merged in where it is used to not have any problems. Because it's guaranteed that `@tunnckocore/release-cli` works both with `new-release` GitHub app and the `@tunnckocore/release` GitHub app. 
    
    Hence, `@tunnckocore/release-cli` is released by `@tunnckocore/release` and published by itself. Also, the `@tunnckocore/release` app is released by `new-release` app and published by `@tunnckocore/release-cli`.
    
    Signed-off-by: Charlike Mike Reagent <mameto2011@gmail.com>

---
+++ cli.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+
+require('@tunnckocore/release-cli/cli');
--- test/index.js
+++ test/index.js
@@ -1,23 +1,6 @@
-/**
- * @copyright 2017-present, Charlike Mike Reagent <olsten.larck@gmail.com>
- * @license Apache-2.0
- */
+import test from 'asia';
+import mod from '../src';
 
-const test = require('mukla')
-const { prepublish, publish, getVersions } = require('../src/index.js')
-
-test('exports an object with "publish" and "prepublish" funciotions', (done) => {
-  test.strictEqual(typeof prepublish, 'function')
-  test.strictEqual(typeof publish, 'function')
-  test.strictEqual(typeof getVersions, 'function')
-  done()
-})
-
-test('getVersions should return an object when options.name is given', async () => {
-  const versions = await getVersions('minor', { name: 'parse-commit-message' })
-  console.log(versions)
-
-  test.strictEqual(typeof versions, 'object')
-  test.strictEqual(typeof versions.lastVersion, 'string')
-  test.strictEqual(typeof versions.nextVersion, 'string')
-})
+test('todo', (t) => {
+  t.strictEqual(typeof mod, 'function');
+});
