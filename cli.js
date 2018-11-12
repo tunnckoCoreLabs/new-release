72a6adc4edbec97af883808d84b98ad77ef49a60
Charlike Mike Reagent <mameto2011@gmail.com>
2018-11-12 15:42:30 +0200

    fix: fukin shit cum on
    Signed-off-by: Charlike Mike Reagent <mameto2011@gmail.com>

--- cli.js
+++ cli.js
@@ -1,3 +1,31 @@
 #!/usr/bin/env node
 
-require('@tunnckocore/release-cli/cli');
+'use strict';
+
+const path = require('path');
+const proc = require('process');
+const parser = require('mri');
+const esmLoader = require('esm');
+
+const pkg = require('./package');
+
+const esmRequire = esmLoader(module);
+
+function interop(x) {
+  if (Object.keys(x).length === 1 && x.default) {
+    return x.default;
+  }
+  return x;
+}
+
+const mod = esmRequire(path.join(__dirname, 'src', 'cli.js'));
+const cli = interop(mod);
+
+const argv = parser(proc.argv.slice(2), {
+  default: {
+    cwd: proc.cwd(),
+    ci: true,
+  },
+});
+
+cli(pkg, argv).catch(console.error);
